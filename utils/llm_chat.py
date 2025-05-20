ae5043daaf1babc9cd87654e897496deb299c4d0
Heisenberg <122362018+abhi2326@users.noreply.github.com>
2025-05-20 13:14:31 +0530

    Update llm_chat.py

--- utils/llm_chat.py
+++ utils/llm_chat.py
@@ -1,8 +1,10 @@
 from langchain.chains import RetrievalQA
 from langchain.llms import HuggingFaceHub
 
-llm = HuggingFaceHub(repo_id="mistralai/Mistral-7B-Instruct-v0.1", model_kwargs={"temperature":0.2})
+# Initialize the language model
+llm = HuggingFaceHub(repo_id="mistralai/Mistral-7B-Instruct-v0.1", model_kwargs={"temperature": 0.2})
 
+# Function to get response from the QA chain
 def get_chat_response(question, retriever):
     qa_chain = RetrievalQA.from_chain_type(llm=llm, retriever=retriever)
-    return qa_chain.run(question)
+    return qa_chain.invoke({"query": question})
